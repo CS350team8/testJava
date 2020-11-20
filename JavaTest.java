637bf0bbbe1e067f1f88859fd5a0a30d4d0d424f
CS350team8 <74752878+CS350team8@users.noreply.github.com>
2020-11-20 12:58:47 +0900

    Add files via upload

---
+++ JavaTest.java
@@ -0,0 +1,16 @@
+public class JavaTest {
+	public static void main(String args[]) {
+		// Credentials
+		String id = "team8";
+		String pw = "password123";
+		
+		// unused variable
+		int temp = 4;
+		
+		// inconsistent indentation
+		int a = 1;
+			int b = 3;
+				int c = a+b;
+		System.out.println("a+b="+c);
+    }
+}
